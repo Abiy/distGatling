45cf228e13ad87edcb2adc6f08dcabcea7d8a4ca
Abiy Hailemichael <Abiy.Hailemichael@walmart.com>
2015-11-02 17:59:07 -0500

    Initial Commit

---
+++ walmart-location-rest/src/main/java/com/walmart/store/location/Application.java
@@ -0,0 +1,17 @@
+package com.walmart.store.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+
+
+
---
+++ walmart-location-rest/src/main/java/com/walmart/store/location/endpoint/v1/LocationController.java
@@ -0,0 +1,32 @@
+package com.walmart.store.location.endpoint.v1;
+
+import com.google.common.collect.ImmutableMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * Created by ahailem on 11/2/15.
+ */
+
+@RestController
+public class LocationController {
+
+    private final Logger log = LoggerFactory.getLogger(LocationController.class);
+
+    @RequestMapping("/home")
+    @ResponseBody
+    Map home() {
+        log.info("Got other request");
+
+        return ImmutableMap.of(
+                "name", "someName",
+                "age", 27
+        );
+    }
+
+}
