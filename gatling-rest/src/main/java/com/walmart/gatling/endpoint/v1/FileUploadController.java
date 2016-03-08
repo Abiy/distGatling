2e09a7b961d561af821aafe5f702e58aa7c0fd26
Abiy Hailemichael <Abiy.Hailemichael@walmart.com>
2016-03-07 19:42:37 -0500

    upload for files completed

---
+++ gatling-rest/src/main/java/com/walmart/gatling/endpoint/v1/FileUploadController.java
@@ -0,0 +1,90 @@
+package com.walmart.gatling.endpoint.v1;
+
+
+
+        import java.io.BufferedOutputStream;
+        import java.io.File;
+        import java.io.FileOutputStream;
+        import java.io.IOException;
+        import java.util.Arrays;
+        import java.util.Comparator;
+        import java.util.List;
+        import java.util.stream.Collectors;
+
+        import org.apache.commons.io.FileUtils;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.util.FileCopyUtils;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.web.bind.annotation.RequestParam;
+        import org.springframework.web.multipart.MultipartFile;
+        import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class FileUploadController {
+public static String  ROOT = "/Users/ahailem/uploadspace/upload/";
+
+    @RequestMapping(method = RequestMethod.GET, value = "/upload")
+    public String provideUploadInfo(Model model) throws IOException {
+        File rootFolder = new File(ROOT);
+        if(rootFolder.exists()) {
+            List<String> fileNames = Arrays.stream(rootFolder.listFiles())
+                    .map(f -> f.getName())
+                    .collect(Collectors.toList());
+        }
+        else {
+            rootFolder.mkdir();
+        }
+        rootFolder = new File(ROOT);
+
+        model.addAttribute("files",
+                Arrays.stream(rootFolder.listFiles())
+                        .sorted(Comparator.comparingLong(f -> -1 * f.lastModified()))
+                        .map(f -> f.getName())
+                        .collect(Collectors.toList())
+        );
+
+        return "uploadForm";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/upload")
+    public String handleFileUpload(@RequestParam("name") String name,
+                                   @RequestParam("role") String role,
+                                   @RequestParam("type") String type,
+                                   @RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        System.out.println("Role:"+role);
+        System.out.println("Type:"+type);
+        if (name.contains("/")) {
+            redirectAttributes.addFlashAttribute("message", "Folder separators not allowed");
+            return "redirect:upload";
+        }
+        if (name.contains("/")) {
+            redirectAttributes.addFlashAttribute("message", "Relative pathnames not allowed");
+            return "redirect:upload";
+        }
+
+        if (!file.isEmpty()) {
+            try {
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(new File(ROOT + "/" + name)));
+                FileCopyUtils.copy(file.getInputStream(), stream);
+                stream.close();
+                redirectAttributes.addFlashAttribute("message",
+                        "You successfully uploaded " + name + "!");
+            }
+            catch (Exception e) {
+                redirectAttributes.addFlashAttribute("message",
+                        "You failed to upload " + name + " => " + e.getMessage());
+            }
+        }
+        else {
+            redirectAttributes.addFlashAttribute("message",
+                    "You failed to upload " + name + " because the file was empty");
+        }
+
+        return "redirect:upload";
+    }
+
+}
