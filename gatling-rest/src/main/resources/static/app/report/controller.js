8dc07565494e6c9ca74ddeb496bc7c4eb6c42575
Abiy Hailemichael <Abiy.Hailemichael@walmart.com>
2016-03-11 13:23:54 -0500

    UI updates for progress

--- gatling-rest/src/main/resources/static/app/report/controller.js
+++ gatling-rest/src/main/resources/static/app/report/controller.js
@@ -5,13 +5,16 @@ angular.module('gatling.report',[]).
     controller('reportCtrl',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
         $scope.trackingId = $routeParams.id;
         $scope.report= '';
+        $scope.inProgress= false;
 
 
           $scope.submitReport = function(){
+                    $scope.inProgress = true;
                    $http.post('/gatling/server/report/'+$scope.trackingId,{})
                    .success(function(data){
                        console.log(data);
                        $scope.report = data.report;
+                       $scope.inProgress = false;
                    });
                }
     }])
