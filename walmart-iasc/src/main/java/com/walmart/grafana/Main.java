2fc8dd568c80f9520b680cee2ba53d917e50c433
Abiy Hailemichael <Abiy.Hailemichael@walmart.com>
2016-05-10 11:46:08 -0400

    refactored main

--- walmart-iasc/src/main/java/com/walmart/grafana/Main.java
+++ walmart-iasc/src/main/java/com/walmart/grafana/Main.java
@@ -1,13 +1,9 @@
 package com.walmart.grafana;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Created by walmart
  */
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) throws InterruptedException {
 
         com.walmart.graylog.LiquiMonitor.configure();
