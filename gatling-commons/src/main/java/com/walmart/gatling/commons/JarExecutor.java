e336f355067894f5fdcff60b95b6c25c860c6c84
Abiy Hailemichael <Abiy.Hailemichael@walmart.com>
2017-11-13 12:34:20 -0500

    more debug statement

--- gatling-commons/src/main/java/com/walmart/gatling/commons/JarExecutor.java
+++ gatling-commons/src/main/java/com/walmart/gatling/commons/JarExecutor.java
@@ -204,13 +204,14 @@ public class JarExecutor extends WorkExecutor {
             //executor.getWatchdog().destroyProcess().
             Worker.Result result = new Worker.Result(exitResult, agentConfig.getUrl(errPath), agentConfig.getUrl(outPath), null, job);
             log.info("Exit code: {}", exitResult);
-            FileUtils.deleteQuietly(FileUtils.getFile(agentConfig.getJob().getJobDirectory(job.jobId, "")));
+            //FileUtils.deleteQuietly(FileUtils.getFile(agentConfig.getJob().getJobDirectory(job.jobId, "")));
             if (executor.isFailure(exitResult) || exitResult == 1) {
                 log.info("Script Executor Failed, job: " + job.jobId);
                 return new Worker.WorkFailed(result);
             } else {
+                log.info("Jar Executor Completed, job: " + result);
                 result = new Worker.Result(exitResult, agentConfig.getUrl(errPath), agentConfig.getUrl(outPath), agentConfig.getUrl(getMetricsPath(job)), job);
-                log.info("Script Executor Completed, job: " + result);
+                log.info("Jar Executor Completed, job: " + result);
                 return new Worker.WorkComplete(result);
             }
 
